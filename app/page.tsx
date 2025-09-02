34fdda9302a8f123a688c5d53dc6516275aab82c
aldottexe <aldottexe@gmail.com>
2025-09-02 02:16:46 -0400

    main interface done

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+"use client"
 import { Bar } from "@/public/components/Bar";
+import { Keypad } from "@/public/components/Keypad";
+import { useState } from "react";
 
 let connections = [
   { name: "Leon", hp: 20, max: 50 },
@@ -6,28 +9,26 @@ let connections = [
   { name: "Mills", hp: 15, max: 20 },
 ];
 
-let user = { name: "freb", hp: 25, max: 30 };
 
 export default function Home() {
+  const [user, setUser] = useState({ name: "freb", hp: 25, max: 30 });
   return (
     <div className="mx-auto mt-20 w-[80%]">
-      <section>
-        <h1 className="text-3xl mb-5">HParty</h1>
 
-        <ul>
-          {connections.map((c, i) => (
-            <li key={i}>
-              <Bar charInfo={c} />
-            </li>
-          ))}
-        </ul>
+      <h1 className="text-3xl mb-5">HParty</h1>
 
-      </section>
-<hr className="my-10"/>
+      <ul>
+        {connections.map((c, i) => (
+          <li key={i}>
+            <Bar charInfo={c} />
+          </li>
+        ))}
+      </ul>
 
-      <section>
-        <Bar charInfo={user} />
-      </section>
+      <hr className="my-5" />
+
+      <Bar charInfo={user} />
+      <Keypad onCommit={(ammt) => setUser({ ...user, hp: Math.max(0, Math.min(user.hp + ammt, user.max)) })} />
     </div>
   );
 }
--- public/components/Bar.tsx
+++ public/components/Bar.tsx
@@ -1,9 +1,9 @@
 export function Bar({ charInfo }: { name: String, hp: Number, max: Number }) {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center gap-5">
       <p className="w-15 text-right">{charInfo.name}</p>
-      <div className="outline h-4 m-2 grow before:absolute before:z-[-1] before:opacity-50 before:h-full before:w-full before:bg-gradient-to-r before:from-orange-300 before:to-teal-300 relative">
-        <div className="h-full overflow-hidden bg-black ml-auto" style={{ width: `${Math.round((charInfo.hp / charInfo.max) * 100)}%` }} >
+      <div className="h-1 m-2 grow before:absolute before:z-[-1] before:opacity-80 before:h-full before:w-full before:bg-gradient-to-r before:from-orange-300 before:to-teal-300 relative">
+        <div className="h-full overflow-hidden bg-black ml-auto transition-all duration-700" style={{ width: `${Math.round(100 - (charInfo.hp / charInfo.max) * 100)}%` }} >
         </div>
       </div>
       <p className="w-15">{charInfo.hp} / {charInfo.max}</p>
